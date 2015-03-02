dc6a09b855b7621cb6bba4382d886bb61c211ed0
Jordan Collier <jordanicollier@gmail.com>
2015-03-02 13:44:08 -0700

    add has_many categorizations to kitten and category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: {case_sensitive: false}
-
-  has_many :kittens, through: :categorization 
+  has_many :categorizations
+  has_many :kittens, through: :categorization
 end
